73482e84067bf19de5f5b6cfcb0c2ba89cd41269
Bristle <bixiao1023.tum@gmail.com>
2018-09-05 20:54:56 +0200

    upgrade

--- example/example.js
+++ example/example.js
@@ -1,18 +1,15 @@
-// const Logger = require('../src/index.js')
-// let loggerWarn = Logger.createLogger('SEC-warn.log')
-// let loggerError = Logger.createLogger('SEC-error.log')
-// let loggerFatal = Logger.createLogger('SEC-fatal.log')
-
 // create and logs to a file
 const logger = require('../src/index.js').createLogger('SEC-out.log')
 
 // set allowing log level, default is info
-logger.setLevel('debug')
 logger.debug('this will be logged now')
 logger.info('this will be logged now')
 logger.error('this will be logged now')
 logger.warn('this will be logged now')
 logger.fatal('this will be logged now')
+
+// Information of levels which are over debugs wil appear
+logger.setLevel('debug')
 
 // format back to information containing timestamp and message
 logger.format()
